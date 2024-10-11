a437cf5207b6cfa6477626cee3f81d318e5ab8f0
DiegoHidenori <diegohidenori.ca@hotmail.com>
2024-10-11 10:55:19 -0400

    Removed node_modules

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+
+// A simple route
+app.get('/', (req, res) => {
+  res.send('Hello World from Express and Docker!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
+module.exports = app;
